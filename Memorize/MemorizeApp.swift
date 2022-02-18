d062566274c773a73acb4e61c1adbc31b4b22925
rishabhpatni <rishabh.p@toppr.com>
2022-02-18 22:00:47 +0530

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by RISHABH PATNI on 18/02/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
